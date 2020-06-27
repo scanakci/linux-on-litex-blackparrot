0c9a6bb570877ae6f8ab1a8ee71ea261ee69f28f
sadullah <sadullahcanakci@gmail.com>
2020-06-27 00:10:16 -0400

    update travis

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd $LITEX/litex/tools
-
-timeout 180s ./litex_sim.py --cpu-type blackparrot --cpu-variant standard --integrated-rom-si    ze 40960 --output-dir build/blackparrot_BIOS > bios.log
+timeout 180s ./litex_sim.py --cpu-type blackparrot --cpu-variant standard --integrated-rom-size 40960 --output-dir blackparrot_BIOS > bios.log
 
 #Test: BIOS terminal pops?
 if grep -rnq 'Console\|No boot medium found' hi; then
