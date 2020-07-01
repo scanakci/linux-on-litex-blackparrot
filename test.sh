6ce526ef47cb0fbef9a91e70c5169317fe339fe9
sadullah <sadullahcanakci@gmail.com>
2020-06-30 23:20:00 -0400

    update travis file

--- test.sh
+++ test.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 cd $LITEX/litex/tools
-timeout 180s ./litex_sim.py --cpu-type blackparrot --cpu-variant standard --integrated-rom-size 40960 --output-dir blackparrot_BIOS > bios.log
 
 #Test: BIOS terminal pops?
-if grep -rnq 'Console\|No boot medium found' hi; then
-  echo "success test1"
+timeout 180s ./litex_sim.py --cpu-type blackparrot --cpu-variant standard --integrated-rom-size 40960 --csr-data-width=32 --output-dir blackparrot_BIOS > bios.log
+
+if grep -rnq 'Console\|No boot medium found' bios.log; then
+  echo "success when running BIOS"
 else
   echo "fail when running BIOS"
+  exit
+
+#Test: Memtest OK using SDRAM model?
+timeout 300s ./litex_sim.py --cpu-type blackparrot -variant standard --integrated-rom-size 40960 --output-dir blackparrot_BIOS_memtest --csr-data-width=32 --sdram-module MT41J256M16 --with-sdram > bios.log
+
+if grep -rnq 'Console\|No boot medium found' bios.log; then
+  echo "Memtest OK in BIOS simulation"
+else
+  echo "Memtest NOT OK in BIOS simulation"
   exit
 
 #Test: Linux boots up?
