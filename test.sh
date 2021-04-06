6f47ed122262c504e74d0825141d99b95f073169
sadullah <sadullahcanakci@gmail.com>
2021-04-06 19:01:40 -0400

    update test.sh

--- test.sh
+++ test.sh
@@ -2,14 +2,15 @@
 cd $LITEX/litex/tools
 
 #Test: BIOS terminal pops?
-timeout 180s ./litex_sim.py --cpu-type blackparrot --cpu-variant sim --output-dir blackparrot_BIOS > bios.log
+timeout 120s ./litex_sim.py --cpu-type blackparrot --cpu-variant sim --output-dir blackparrot_BIOS > bios.log
 
+cat bios.log
 if grep -rnq 'Console\|No boot medium found' bios.log; then
   echo "success when running BIOS"
 else
   echo "fail when running BIOS"
   exit
-
+fi
 #Test: Memtest OK using SDRAM model?
 timeout 300s ./litex_sim.py --cpu-type blackparrot -variant standard --integrated-rom-size 40960 --output-dir blackparrot_BIOS_memtest --csr-data-width=32 --sdram-module MT41J256M16 --with-sdram > bios.log
 
@@ -18,5 +19,5 @@ if grep -rnq 'Console\|No boot medium found' bios.log; then
 else
   echo "Memtest NOT OK in BIOS simulation"
   exit
-
+fi
 #Test: Linux boots up?
